4483f3ce749851b8d71b00e52d0b1b902f3985e2
Peng Huang <shawn.p.huang@gmail.com>
2012-03-23 14:58:37 -0400

    wip

--- appsv2/upnp/upnp.js
+++ appsv2/upnp/upnp.js
@@ -1,48 +1,125 @@
 // vim: et sts=2 ts=2 :
 (function(exports) {
 
-  function UPNPServer(url, callback) {
-    this.init_ = false;
-    this.url_ = url;
-    this.cdService_ = null;
-    this.cmService_ = null;
+  const CONNECTION_MANAGER = "urn:schemas-upnp-org:service:ConnectionManager:1";
+  const CONTENT_DIRECTORY = "urn:schemas-upnp-org:service:ContentDirectory:1";
 
-    this._init(callback);
+  function UPNPServer(url, callback) {
+    var self = this;
+    this.device_ = null;
+
+    init(callback);
+    function init(callback) {
+      $.get(url, function(xml) {
+        var $xml = $(xml);
+        self.URLBase_ = $xml.find("URLBase").text();
+        // Parses the device.
+        var $device = $xml.find("device").first();
+        if ($device.length != 1) {
+          callback(null);
+          return;
+        }
+
+        function Device($device) {
+          var self = this;
+          this.deviceType = $device.find("deviceType").text();
+          this.presentationURL = $device.find("presentationURL").text();
+          this.friendlyName = $device.find("friendlyName").text();
+          this.manufacturer = $device.find("manufacturer").text();
+          this.manufacturerURL = $device.find("manufacturerURL").text();
+          this.modelDescription = $device.find("modelDescription").text();
+          this.modelName = $device.find("modelName").text();
+          this.modelNumber = $device.find("modelNumber").text();
+          this.serialNumber = $device.find("serialNumber").text();
+          this.UDN = $device.find("UDN").text();
+          this.UDN = $device.find("UDN").text();
+          this.icons = [];
+          this.services = {};
+
+          // Parses icons.
+          $device.find("icon").each(function() {
+            var $this = $(this);
+            var icon = {
+                mimetype: $this.find("mimetype").text(),
+                width: Number($this.find("width").text()),
+                height: Number($this.find("height").text()),
+                depth: Number($this.find("depth").text()),
+                url: $(this).find("url").text(),
+            };
+            self.icons.push(icon);
+          });
+
+          // Parses services.
+          $device.find("ServiceList service").each(function() {
+            var $this = $(this);
+            var service = {
+                serviceType: $this.find("serviceType").text(),
+                serviceId: $this.find("serviceId").text(),
+                scpdURL: $this.find("SCPDURL").text(),
+                controlURL: $this.find("controlURL").text(),
+                eventSubURL: $this.find("eventSubURL").text(),
+            };
+            self.services[service.serviceType] = service;
+          });
+
+        }
+
+        self.device_ = new Device($device);
+        callback(self);
+      });
+    }
   }
 
-  UPNPServer.prototype._init = function(callback) {
-    _this = this;
-    $.get(url, function(xml) {
-      _this.descritption_ = xmlToJson(data);
-
-      var services = _this.description_.root.device.serviceList;
-      for (var i = 0; i < services.lenght; i++) {
-        switch(services[i].serviceId) {
-          case "urn:upnp-org:serviceId:ContentDirectory":
-            _this.cdService = service;
-            breal;
-          case "urn:upnp-org:serviceId:ConnectionManager":
-            _this.cmService = service;
-            break;
-        }
-      }
-      callback(true);
-    });
-  };
-
   UPNPServer.prototype.makeURL = function(path) {
-    return this.url_ + path;
+    return this.URLBase_ + path.substring(1);
   };
 
-  UPNPServer.prototype.getIcon = function() {
-    return this.makeURL(this.description_.root.device.iconList[0].url);
+  UPNPServer.prototype.getIcon = function(width, height, mimetypes) {
+    var url = "";
+    if (this.device_ != null) {
+      var $icons = $(this.device_.icons);
+      $icons = $icons.filter(function(){
+        if ($.inArray(this.mimetype, mimetypes) < 0)
+          return false;
+        if (width > 0 && this.width > width)
+          return false;
+        if (height > 0 && this.height > height)
+          return false;
+        return true;
+      });
+
+      var best = null;
+      $icons.each(function(){
+        if (best != null) {
+          if (width <= height) {
+            if (this.width > best.width)
+              best = this;
+          } else {
+            if (this.height > best.height)
+              best = this;
+          }
+        } else {
+          best = this;
+        }
+      });
+      url = this.makeURL(best.url);
+    }
+    return url;
   };
 
-  UPNPServer.prototype.browser = function(id, start, count, callback) {
-    if (this.cdService_ == null)
+  UPNPServer.prototype.browse = function(id, start, count, callback) {
+    if (this.device_ == null) {
+      callback(null);
       return false;
-    
-    var url = this.makeURL(this.cdService_.cc);
+    }
+
+    var service = this.device_.services[CONTENT_DIRECTORY];
+    if (service == null) {
+      callback(null);
+      return false;
+    }
+
+    var url = this.makeURL(service.controlURL);
     var params = new SOAPClientParameters();
     params.add("ObjectID", id);
     params.add("BrowseFlag", "BrowseDirectChildren");
@@ -51,16 +128,59 @@
     params.add("RequestedCount", count);
     params.add("SortCriteria", "");
 
-    var namespace = "urn:schemas-upnp-org:service:ContentDirectory:1";
-    SOAPClient._sendSoapRequestInternal(url,namespace, "Browse", params, true, function(o, r) {
-       
-      console.log(r);
+    SOAPClient._sendSoapRequestInternal(url, CONTENT_DIRECTORY, "Browse",
+        params, true, function(o, xml){
+      var $xml = $(xml);
+      var $result = $($.parseXML($xml.find("Result").text()));
+      var items = [];
+      $result.find("DIDL-Lite").first().children().each(function(){
+        function Item($item){
+          var item = $item[0];
+          this.class = $item.find("class").text();
+          this.title = $item.find("title").text();
+          this.id = Number(item.getAttribute("id"));
+          this.parentID = Number(item.getAttribute("parentID"));
+          this.restricted = Boolean(item.getAttribute("restricted"));
+          this.childCount = Number(item.getAttribute("childCount"));
+          console.log($item);
+
+          if (this.class !== "object.container") {
+            this.artist = $item.find("artist").text();
+            this.album = $item.find("album").text();
+            this.date = $item.find("date").text();
+            this.genre = $item.find("genre").text();
+            this.description = $item.find("description").text();
+            this.originalTrackNumber = Number($item.find("originalTrackNumber").text());
+            this.albumArtURI = $item.find("albumArtURI").text();
+
+            function Res($res) {
+              var res = $res[0];
+              this.URL = $res.text();
+              this.protocolInfo = res.getAttribute("protocolInfo");
+              this.size = Number(res.getAttribute("size"));
+              this.duration = res.getAttribute("duration");
+              this.bitrate = Number(res.getAttribute("bitrate"));
+              this.resolution = res.getAttribute("resulation");
+              this.sampleFrequency = Number(res.getAttribute("sampleFrequency"));
+              this.nrAudioChannels = Number(res.getAttribute("nrAudioChannels"));
+            }
+
+            this.res = new Res($item.find("res").first());
+          }
+        }
+        items.push(new Item($(this)));
+      });
+
+      callback(items);
+
     }, null);
     return true;
   };
 
   UPNPServer.prototype.soapCall = function(obj, callback) {
   };
+
+  exports.UPNPServer = UPNPServer;
 
   function T() {
     this.value = 1;
@@ -93,7 +213,7 @@
 
 })(window);
 
-/* 
+/*
 function m_search() {
   var address = '239.255.255.250';
   var port = 1900;
