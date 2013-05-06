49382766db153d5cc4690866ebfc9d9c1c93e0ca
Peng Huang <penghuang@penghuang-macbookpro.local>
2013-05-05 23:05:07 -0400

    wip

--- AdbDaemon/src/org/chromium/alloy/adb/IOChannel.java
+++ AdbDaemon/src/org/chromium/alloy/adb/IOChannel.java
@@ -3,19 +3,10 @@ package org.chromium.alloy.adb;
 import java.io.IOException;
 
 interface IOChannel {
-  /**
-   * @throws IOException
-   */
   public boolean onAcceptable();
 
-  /**
-   * @throws IOException
-   */
   public boolean onReadable();
 
-  /**
-   * @return TODO
-   */
   public boolean onWritable();
 
   public void onClose();
