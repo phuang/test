838351c44db2e11a4f80fd5f93edcb654dcff918
Peng Huang <penghuang@chromium.org>
2019-09-19 15:19:11 -0400

    WIP

--- math/math_gen.py
+++ math/math_gen.py
@@ -1,14 +1,15 @@
 import random
 
-RESULT_LIMIT = 10
+RESULT_LIMIT = 12
+M = 10
 N = 45
 
 random.seed()
 
 questions = []
 while len(questions) < N:
-  a = random.randint(1, N)
-  b = random.randint(1, N)
+  a = random.randint(1, M)
+  b = random.randint(1, M)
   if a + b > RESULT_LIMIT:
     continue
   q = "%d + %d =" % (a, b)
