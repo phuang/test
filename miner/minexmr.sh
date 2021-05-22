4ef4b4deecd1d419235ffafff2dfc5e6b715f037
Peng Huang <shawn.p.huang@gmail.com>
2021-05-22 08:09:34 -0400

    WIP

--- miner/minexmr.sh
+++ miner/minexmr.sh
@@ -1,1 +1,1 @@
-./xmrig -o ca.minexmr.com:443 -u 46eMo36758GWjQevbyJPnPc9ch7VAGd7T6uR6sqRTTGuRdacTup88exWcaz9NeUchCW27zsiyooj5P6pXpy1R65L98zuYVG -k --tls --rig-id aaaa
+./xmrig -o ca.minexmr.com:443 -u 46eMo36758GWjQevbyJPnPc9ch7VAGd7T6uR6sqRTTGuRdacTup88exWcaz9NeUchCW27zsiyooj5P6pXpy1R65L98zuYVG -k --tls --rig-id xps-8940-4
